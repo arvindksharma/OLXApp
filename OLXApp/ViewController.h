3778028cf78d2e65fa913ffcbde5e13e364ef669
Arvind Sharma <arvind.ksharma01@gmail.com>
2015-09-26 11:40:13 +0530

    Initial Commit

---
+++ OLXApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OLXApp
+//
+//  Created by Arvind Sharma on 26/09/15.
+//  Copyright © 2015 Arvind Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OLXApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OLXApp
+//
+//  Created by Arvind Sharma on 26/09/15.
+//  Copyright © 2015 Arvind Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
